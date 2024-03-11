297e069c07dbb435240e4e5ae8dd64b4ee0b11d2
Ville Takanen <vitkukissa@gmail.com>
2024-03-11 23:39:45 +0200

    fix: basic stats support added

--- src/components/solid/character/CharacterSheet.tsx
+++ src/components/solid/character/CharacterSheet.tsx
@@ -1,5 +1,6 @@
-import { Component } from 'solid-js';
+import type { Component } from 'solid-js';
 import type { Character } from '../../../schema/Character';
+import { t } from 'i18next';
 
 interface CharacterSheetProps {
     character: Character;
@@ -10,9 +11,29 @@ const CharacterSheet: Component<CharacterSheetProps> = (props) => {
     // Add your component logic here
 
     return (
+        <div>
         <h4>
             {props.character.name}
         </h4>
+        <div class="flex">
+        {/* Iterate through stat blocks */}
+        {props.character.statBlocks && 
+          props.character.statBlocks.map((statBlock) => (
+            <div data-key={statBlock.name}> 
+                <h4>{t(`stats:${statBlock.name}`)}</h4> {/* Localized stat block title */}
+
+                {/* Iterate through attributes in the block */}
+                <ul>
+                    {statBlock.attributes && Object.entries(statBlock.attributes).map(([attrName, attrValue]) => (
+                        <li data-key={attrName}>
+                            <strong>{t(`stats:${attrName}`)}: </strong> {attrValue} {/* Localized attribute */}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        ))}
+        </div>
+    </div>
     );
 };
 
--- src/schema/Character.ts
+++ src/schema/Character.ts
@@ -1,10 +1,22 @@
 import { z } from 'zod'
 
+/**
+ * A Player character for a role-playing game
+ */
 export const CharacterSchema = z.object({
     key: z.string(),
     name: z.string(),
     flowTime: z.number(),
     markdownContent: z.string().optional(),
+    // The character's stats are a record of key-value pairs,
+    // where the key is the name of the stat and the value is the stat's value
+    // Each stat belongs to a category (e.g. "Attributes", "Skills", "Abilities")
+    statBlocks: z.array(
+      z.object({
+          name: z.string(),
+          attributes: z.record(z.string()).optional(), 
+      })
+    ).optional(),
   })
   
 export type Character = z.infer<typeof CharacterSchema>
