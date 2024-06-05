bf0988df93ba218818b1d7763256fe25df0354c0
Ville Takanen <vitkukissa@gmail.com>
2024-06-05 15:20:36 +0300

    uid persisted to local storage

--- src/components/client/ProfileButton/ProfileButton.tsx
+++ src/components/client/ProfileButton/ProfileButton.tsx
@@ -1,8 +1,31 @@
+import { makePersisted } from '@solid-primitives/storage';
 import { t } from '@utils/i18n';
-import type { Component } from 'solid-js';
+import { logDebug } from '@utils/logHelpers';
+import { createSignal, type Component } from 'solid-js';
+import { auth } from 'src/firebase/client';
 
 export const ProfileButton: Component = (props) => {
-  return (
+  const [uid, setUid] = makePersisted(
+    createSignal(''), 
+    {
+      name: "uid"
+    });
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUid(user.uid);
+      logDebug('User', user.uid, ' logged in');
+    } else {
+      logDebug('User state changed to anonymous');
+      setUid('');
+    }
+  })
+
+  return uid() ? (
+    <a href="/settings">
+      <cn-navigation-icon noun="avatar" label={t('navigation:settings')} />
+    </a>
+  ) : (
     <a href="/login">
       <cn-navigation-icon noun="login" label={t('navigation:login')} />
     </a>
