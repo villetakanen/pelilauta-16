48432a3930245a8dbb8010f14b5a8d280d6c5572
Ville Takanen <vitkukissa@gmail.com>
2024-06-05 11:16:45 +0300

    fix: biome

--- src/components/client/ProfileButton/ProfileButton.tsx
+++ src/components/client/ProfileButton/ProfileButton.tsx
@@ -1,10 +1,10 @@
-import type { Component } from 'solid-js';
 import { t } from '@utils/i18n';
+import type { Component } from 'solid-js';
 
 export const ProfileButton: Component = (props) => {
   return (
     <a href="/login">
       <cn-navigation-icon noun="login" label={t('navigation:login')} />
     </a>
-  )
-}
+  );
+};
--- src/lit-shims.d.ts
+++ src/lit-shims.d.ts
@@ -1,31 +1,31 @@
 import {
-  CnCard,
-  CnIcon,
-  CnNavigationIcon,
+  type CnAvatar,
   CnAvatarButton,
   CnBubble,
-  CnAvatar,
-  CnReactionButton,
+  CnCard,
   CnDialog,
-} from '@11thdeg/cyan-next'
+  CnIcon,
+  CnNavigationIcon,
+  type CnReactionButton,
+} from '@11thdeg/cyan-next';
 declare module 'solid-js' {
   namespace JSX {
     interface IntrinsicElements {
-      'cn-icon': HTMLAttributes & { noun: string }
-      'cn-card': HTMLAttributes
-      'cn-dialog': HTMLAttributes
-      'cn-navigation-icon': HTMLAttributes & { noun: string; label: string }
-      'cn-avatar-button': HTMLAttributes & { src: string }
-      'cn-avatar': CnAvatar
-      'cn-bubble': HTMLAttributes
-      'cn-reaction-button': CnReactionButton
-      'cn-loader': HTMLAttributes
+      'cn-icon': HTMLAttributes & { noun: string };
+      'cn-card': HTMLAttributes;
+      'cn-dialog': HTMLAttributes;
+      'cn-navigation-icon': HTMLAttributes & { noun: string; label: string };
+      'cn-avatar-button': HTMLAttributes & { src: string };
+      'cn-avatar': CnAvatar;
+      'cn-bubble': HTMLAttributes;
+      'cn-reaction-button': CnReactionButton;
+      'cn-loader': HTMLAttributes;
       'cn-pill': HTMLAttributes & {
-        label: string
-        noun: string
-        checked: boolean
-        value: string
-      }
+        label: string;
+        noun: string;
+        checked: boolean;
+        value: string;
+      };
     }
   }
 }
