d9922e09329614eb297f3f88179238d2157d73ee
Ville Takanen <vitkukissa@gmail.com>
2024-07-01 12:15:22 +0300

    feat: toc page added

---
+++ src/components/client/sites/SiteApp/SiteTocPage.tsx
@@ -0,0 +1,10 @@
+import type { Component } from 'solid-js';
+
+export const SiteTocPage: Component = () => {
+  return (
+    <div>
+      <h1>Site Table of Contents</h1>
+      <p>This is the table of contents for the site.</p>
+    </div>
+  );
+};
