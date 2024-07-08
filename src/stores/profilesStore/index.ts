6ac2f126d0462fe58be28992bb8a02b3e182672f
Ville Takanen <vitkukissa@gmail.com>
2024-07-08 13:00:57 +0300

    feat: added a store for public profiles

---
+++ src/stores/profilesStore/index.ts
@@ -0,0 +1,73 @@
+import { persistentAtom } from '@nanostores/persistent';
+import { t } from '@utils/i18n';
+import { logDebug } from '@utils/logHelpers';
+import { doc, getDoc } from 'firebase/firestore';
+import { atom } from 'nanostores';
+import { db } from 'src/firebase/client';
+import { z } from 'zod';
+
+const PublicProfileSchema = z.object({
+  key: z.string(),
+  nick: z.string(),
+  avatarURL: z.string().optional(),
+});
+
+export type PublicProfile = z.infer<typeof PublicProfileSchema>;
+
+/**
+ * Store for public profiles
+ *
+ * This store is used to store public profiles of users, instead of directly using the store
+ * as a reactive store, we use it in createEffect, so we may update a profile to existing when it
+ * is fetched from the server.
+ */
+export const $profiles = persistentAtom<Record<string, PublicProfile>>(
+  'profiles',
+  {},
+  {
+    encode: JSON.stringify,
+    decode: (data) => {
+      const object = JSON.parse(data);
+      return object;
+    },
+  },
+);
+
+const $loading = atom<string[]>([]);
+
+export function getProfile(key: string): PublicProfile | undefined {
+  if ($profiles.get()[key]) {
+    return $profiles.get()[key];
+  }
+  // background fetch for the profile
+  fetchProfile(key);
+}
+
+async function fetchProfile(key: string) {
+  if ($loading.get().includes(key)) {
+    return;
+  }
+  $loading.set([...$loading.get(), key]);
+  const publicProfileDoc = await getDoc(doc(db, 'profiles', key));
+  if (publicProfileDoc.exists()) {
+    const publicProfile = PublicProfileSchema.parse(publicProfileDoc.data());
+    $profiles.set({
+      ...$profiles.get(),
+      [key]: publicProfile,
+    });
+  } else {
+    logDebug(
+      'profilesStore',
+      'fetchProfile',
+      'no profile found, assuming anonymous user',
+    );
+    $profiles.set({
+      ...$profiles.get(),
+      [key]: {
+        key,
+        nick: t('app:meta.anonymous'),
+      },
+    });
+  }
+  $loading.set($loading.get().filter((k) => k !== key));
+}
