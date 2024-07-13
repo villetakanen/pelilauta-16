d23fd1e0c2a10d28aff98baa303a374132ca475b
Ville Takanen <vitkukissa@gmail.com>
2024-07-13 09:14:01 +0300

    feat: page route added

--- src/components/client/sites/SiteApp/SiteTocPage.tsx
+++ src/components/client/sites/SiteApp/SiteTocPage.tsx
@@ -1,21 +1,36 @@
 import { useStore } from '@nanostores/solid';
-import { type Component, For } from 'solid-js';
+import { type Component, For, createMemo } from 'solid-js';
+import { $site } from 'src/stores/activeSiteStore';
 import { $pages } from 'src/stores/activeSiteStore/pagesStore';
 
 export const SiteTocPage: Component = () => {
   const pages = useStore($pages);
+  const site = useStore($site);
+
+  const inAlphabeticalOrder = createMemo(() => {
+    const sorted = [...pages()];
+    sorted.sort((a, b) => a.name.localeCompare(b.name));
+    return sorted;
+  });
 
   return (
-    <div>
-      <h1>Site Table of Contents</h1>
-      <p>This is the table of contents for the site.</p>
-      <For each={pages()}>
-        {(page) => (
-          <div>
-            <h4>{page.name}</h4>
-          </div>
-        )}
-      </For>
+    <div class="content-columns">
+      <div>
+        <h1>Site Table of Contents</h1>
+        <p>This is the table of contents for the site.</p>
+      </div>
+
+      <div>
+        <ul>
+          <For each={inAlphabeticalOrder()}>
+            {(page) => (
+              <li>
+                <a href={`/sites/${site().key}/${page.key}`}>{page.name}</a>
+              </li>
+            )}
+          </For>
+        </ul>
+      </div>
     </div>
   );
 };
