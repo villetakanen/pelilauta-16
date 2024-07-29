267fdf48ca19da991d1d68fc7f3a43ed88033c90
Ville Takanen <vitkukissa@gmail.com>
2024-07-29 11:08:26 +0300

    fix: category search for forum front page

--- src/components/client/threads/ForumPage.tsx
+++ src/components/client/threads/ForumPage.tsx
@@ -1,5 +1,25 @@
-import type { Component } from 'solid-js';
+import { useStore } from '@nanostores/solid';
+import { $topics } from '@stores/ThreadsApp/topics';
+import { type Component, For, createMemo } from 'solid-js';
 
 export const ForumPage: Component = () => {
-  return <div class="content-columns">[ForumPage]</div>;
+  const topics = useStore($topics);
+
+  const categories = createMemo(() => {
+    const cat = ['Pelilauta'];
+    for (const topic of topics()) {
+      if (Object.keys(topic).includes('category')) {
+        cat.push(`${topic.category}`);
+      }
+    }
+    return cat;
+  });
+
+  return (
+    <div class="content-columns">
+      <div class="column-l">
+        <For each={categories()}>{(category) => <p>{category}</p>}</For>
+      </div>
+    </div>
+  );
 };
