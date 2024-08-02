e159628d58fee905983b84ba30ab49a8e191ee12
Ville Takanen <vitkukissa@gmail.com>
2024-08-02 10:27:15 +0300

    fix: fetching first page works

--- src/stores/channelStore/channelStore.ts
+++ src/stores/channelStore/channelStore.ts
@@ -6,6 +6,7 @@ import {
 } from '@schemas/ThreadSchema';
 import { $topics } from '@stores/ThreadsApp/topics';
 import { toClientEntry } from '@utils/client/entryUtils';
+import { logDebug } from '@utils/logHelpers';
 import {
   collection,
   doc,
@@ -34,7 +35,9 @@ export const $channel = persistentAtom<Thread[]>('active-channel', [], {
 export const $channelKey = atom<string | null>(null);
 
 export async function fetchPage(key: string, page = 1) {
+  logDebug('fetchPage', key, page);
   if ($channelKey.get() === key) {
+    logDebug('fetchPage', 'current channel key matches the requested key');
     // We'll check if we have the data in the cache, and if we do, we'll return it
     const requrestedCacheSize = CHANNEL_PAGE_SIZE * page;
     const maximumCacheSize =
@@ -42,6 +45,11 @@ export async function fetchPage(key: string, page = 1) {
     const expectedCacheSize = Math.min(requrestedCacheSize, maximumCacheSize);
 
     while ($channel.get().length < expectedCacheSize) {
+      logDebug(
+        'fetchPage',
+        'fetching more data from Firestore, cache size:',
+        $channel.get().length,
+      );
       $channel.set([
         ...$channel.get(),
         ...(await fetchPageFromFirestore(
@@ -55,14 +63,35 @@ export async function fetchPage(key: string, page = 1) {
       .get()
       .slice(CHANNEL_PAGE_SIZE * (page - 1), CHANNEL_PAGE_SIZE * page);
   }
-  return [];
+  if (!key) return [];
+
+  logDebug('fetchPage', 'fetching new channel data from Firestore to cache');
+  $channelKey.set(key);
+
+  const newThreads = await fetchPageFromFirestore(key);
+  $channel.set(newThreads);
+
+  logDebug(
+    'fetchPage',
+    'returning the first page of the new channel data, cache size:',
+    newThreads.length,
+  );
+  return newThreads;
 }
+
 async function fetchPageFromFirestore(
   channelKey: string,
   fromThreadKey?: string,
 ) {
+  logDebug('fetchPageFromFirestore', channelKey, fromThreadKey);
+
   // if we do not have the fromThreadKey, we'll fetch the beginning of the channel
   if (!fromThreadKey) {
+    logDebug(
+      'fetchPageFromFirestore',
+      'fetching the beginning of the channel',
+      channelKey,
+    );
     const q = query(
       collection(db, THREADS_COLLECTION_NAME),
       orderBy('flowTime', 'desc'),
@@ -73,7 +102,7 @@ async function fetchPageFromFirestore(
 
     const newThreads: Thread[] = [];
     for (const doc of pageResults.docs) {
-      newThreads.push(ParseThread(doc.data(), doc.id));
+      newThreads.push(ParseThread(toClientEntry(doc.data()), doc.id));
     }
 
     return newThreads;
