b57de673dea186a1ad366af95e3fa332bd6ac557
Ville Takanen <vitkukissa@gmail.com>
2024-08-12 11:17:55 +0300

    fix: thread editor topics are fetched from db

---
+++ src/components/client/threads/ThreadEditorApp/ThreadEditorApp.tsx
@@ -0,0 +1,38 @@
+import { useStore } from "@nanostores/solid";
+import { $topics } from "@stores/ThreadsApp/topics";
+import { t } from "@utils/i18n";
+import { createSignal, For, type Component } from "solid-js";
+
+export const ThreadEditorApp:Component<{threadKey?: string}> = () => {
+
+  const topics = useStore($topics);
+
+  const [topic, setTopic] = createSignal<string>();
+
+  return (
+    <form class="content-editor">
+      <div class="toolbar">
+        <label>{t('entries:thread.title')}
+          <input type="text" placeholder={t('entries:thread.placeholders.title')} />
+        </label>
+        <label>{t('entries:thread.title')}
+          <select value={topic()}>
+            <For each={topics()}>
+              {(topic) => (
+                <option value={topic.slug}>{topic.name}</option>
+              )}
+            </For>
+          </select>
+        </label>
+      </div>
+      
+      <label>{t('entries:thread.content')}
+        <textarea />
+      </label>
+      <div class="toolbar">
+        <button type="reset">{t('actions:cancel')}</button>
+        <button type="submit">{t('actions:send')}</button>
+      </div>
+    </form>
+  )
+}
