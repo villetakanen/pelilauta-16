59f839a4ac9da111f0b4ab88e5c823178970ccb4
Ville Takanen <vitkukissa@gmail.com>
2024-08-12 14:05:46 +0300

    fix: updated cyan for editor

--- src/components/client/threads/ThreadEditorApp/ThreadEditorApp.tsx
+++ src/components/client/threads/ThreadEditorApp/ThreadEditorApp.tsx
@@ -11,7 +11,7 @@ export const ThreadEditorApp: Component<{ threadKey?: string }> = () => {
   return (
     <form class="content-editor">
       <div class="toolbar">
-        <label>
+        <label class="flex-grow">
           {t('entries:thread.title')}
           <input
             type="text"
@@ -20,21 +20,34 @@ export const ThreadEditorApp: Component<{ threadKey?: string }> = () => {
         </label>
         <label>
           {t('entries:thread.title')}
-          <select value={topic()}>
+          <select
+            value={topic()}
+            onChange={(e) => setTopic(e.currentTarget.value)}
+          >
             <For each={topics()}>
               {(topic) => <option value={topic.slug}>{topic.name}</option>}
             </For>
           </select>
         </label>
+        <button type="button">
+          <cn-icon noun="add" />
+        </button>
       </div>
 
       <label>
         {t('entries:thread.content')}
         <textarea />
       </label>
+
+      <div class="debug">
+        topic: ({topic()})</div>
+
       <div class="toolbar">
         <button type="reset">{t('actions:cancel')}</button>
-        <button type="submit">{t('actions:send')}</button>
+        <button type="submit">
+          <cn-icon noun="send" />
+          <span>{t('actions:send')}</span>
+        </button>
       </div>
     </form>
   );
