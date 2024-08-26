705db88994d265a3319aefb265192d38a07b19ed
Ville Takanen <vitkukissa@gmail.com>
2024-08-26 09:43:13 +0300

    chore: added playwright

--- vitest.config.js
+++ vitest.config.js
@@ -3,4 +3,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    // Include test files, test fot the test files, spec for playwright e2e tests
+    include: ['**/*.test.ts'],
+  },
 });
