bc9e975465d3d55bf9eccf3918960e2c5030f368
Ville Takanen <vitkukissa@gmail.com>
2024-08-27 13:58:07 +0300

    fix: empty cover to undefined in cards

--- src/components/client/threads/ThreadCard.tsx
+++ src/components/client/threads/ThreadCard.tsx
@@ -14,7 +14,7 @@ export const ThreadCard: Component<{
   elevation?: number;
 }> = (props) => {
   const notify = createMemo(() => props.notify);
-
+ 
   return (
     <div style="flex-basis: auto;">
       <cn-card
@@ -22,7 +22,7 @@ export const ThreadCard: Component<{
         href={`/threads/${props.thread.key}`}
         noun={topicToNoun(props.thread.channel)}
         title={props.thread.title}
-        cover={props.thread.poster}
+        cover={props.thread.poster || undefined}
         elevation={props.elevation}
       >
         <div class="downscaled">
