932448359e03426e7d17a3a58ecb457c563a6913
Ville Takanen <vitkukissa@gmail.com>
2024-09-26 15:17:18 +0300

    feat: ssr db in place

--- src/firebase/server/ThisFolderIntentionallyEmpty.md
+++
@@ -1,4 +0,0 @@
-# Server side Firebase connectivity
-
-*Deferred to v17 for simplicity and to avoid confusion. Will be added in a future release.*
-
---
+++ src/firebase/server/index.ts
@@ -0,0 +1,26 @@
+import type { ServiceAccount } from 'firebase-admin';
+import admin from 'firebase-admin';
+import { cert, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+const serviceAccount = {
+  type: 'service_account',
+  project_id: import.meta.env.FIREBASE_PROJECT_ID,
+  private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
+  client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
+  client_id: import.meta.env.FIREBASE_CLIENT_ID,
+  auth_uri: import.meta.env.FIREBASE_AUTH_URI,
+  token_uri: import.meta.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
+  client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+};
+
+if (!admin.apps.length) {
+  initializeApp({
+    credential: cert(serviceAccount as ServiceAccount),
+  });
+}
+
+export const serverApp = admin.app(); // Reuse existing instance
+export const serverDB = getFirestore();
