69ec1e8ad24ead36bd6ea0a12ef4d6b561eb6a78
Ville Takanen <vitkukissa@gmail.com>
2024-09-30 10:55:42 +0300

    fix: replyfomr refactor to replydialog

--- src/components/client/ThreadsApp/ReplyButton.tsx
+++ src/components/client/ThreadsApp/ReplyButton.tsx
@@ -1,31 +1,53 @@
+import { useStore } from '@nanostores/solid';
+import type { Reply } from '@schemas/ReplySchema';
+import { createEventDispatcher } from '@solid-primitives/event-dispatcher';
+import { $uid } from '@stores/sessionStore';
+import { t } from '@utils/i18n';
+import type { Atom } from 'nanostores';
 /**
  * A Solid-js component that renders a button that opens a dialog to reply to a thread.
  *
  * This component is used in the ThreadsApp component, it injects the ReplyDialog component
  * to the #base-tail div at the end of each astro-layout component.
  */
-
-import type { Thread } from '@schemas/ThreadSchema';
 import { type Component, createSignal } from 'solid-js';
-import { Portal } from 'solid-js/web';
 import { ReplyDialog } from './ReplyDialog';
 
 interface Props {
-  thread?: Thread;
+  quoteRef?: string;
+  threadKey: string;
+  discussion?: Atom<Reply[]>;
+  onQuote: (evt: CustomEvent<string>) => void;
   onClose?: () => void;
 }
 
 export const ReplyButton: Component<Props> = (props) => {
+  const uid = useStore($uid);
   const [showModal, setShowModal] = createSignal(false);
+
+  function handleClose() {
+    setShowModal(false);
+    props.onClose?.();
+  }
 
   return (
     <>
-      <button type="button" onClick={() => setShowModal(true)}>
-        Reply
-      </button>
-      <Portal mount={document.querySelector('#base-tail') as Node}>
-        <ReplyDialog {...props} open={showModal()} />
-      </Portal>
+      {' '}
+      {uid() && (
+        <>
+          <button type="button" onClick={() => setShowModal(true)}>
+            <cn-icon noun="send" />
+            <span class="hide-on-mobile">{t('actions:reply')}</span>
+          </button>
+          <ReplyDialog {...props} open={showModal()} />
+        </>
+      )}
+      {!uid() && (
+        <a href="/login" class="button">
+          <cn-icon noun="avatar" />
+          <span class="hide-on-mobile">{t('actions:login')}</span>
+        </a>
+      )}
     </>
   );
 };
--- src/components/client/ThreadsApp/ReplyDialog.tsx
+++ src/components/client/ThreadsApp/ReplyDialog.tsx
@@ -1,5 +1,5 @@
 /**
- * A cn-dialog component that allows the user to reply to a thread.
+ * A dialog component that allows the user to reply to a thread.
  *
  * Do note: we moved replying to a dialog to match the design of Threads, Linker,
  * and other social media platforms.
@@ -7,43 +7,109 @@
  * The theory here is that people find it more intuitive to use a similar interface
  * across different platforms.
  */
-import type { CnDialog } from '@11thdeg/cyan-next';
-import type { Thread } from '@schemas/ThreadSchema';
+import { ProfileLink } from '@client/shared/ProfileLink';
+import { useStore } from '@nanostores/solid';
+import { type Reply, createReply } from '@schemas/ReplySchema';
+import { createEventDispatcher } from '@solid-primitives/event-dispatcher';
+import { addReply } from '@stores/ThreadsApp/discussion';
+import { $uid } from '@stores/sessionStore';
+import { t } from '@utils/i18n';
 import { logDebug } from '@utils/logHelpers';
+import { type Atom, atom } from 'nanostores';
 import { type Component, createEffect, createSignal } from 'solid-js';
+import { MarkdownSection } from 'src/components/shared/MarkdownSection';
 
 interface Props {
-  thread?: Thread;
   open: boolean;
+  quoteRef?: string;
+  threadKey: string;
+  discussion?: Atom<Reply[]>;
+  onQuote: (evt: CustomEvent<string>) => void;
   onClose?: () => void;
 }
 
 export const ReplyDialog: Component<Props> = (props) => {
-  let dialogRef: CnDialog | undefined;
+  const uid = useStore($uid);
+  let dialog: HTMLDialogElement | undefined;
+  const [message, setMessage] = createSignal<string | null>(null);
+  const dispatch = createEventDispatcher(props);
+  const discussion = useStore(props.discussion || atom([]));
+  const quote = () =>
+    discussion().find((reply) => reply.key === props.quoteRef);
 
   createEffect(() => {
-    logDebug('ReplyDialog', 'open', props.open);
     if (props.open) {
-      const dialog = dialogRef;
-      if (dialog) {
-        (dialog as CnDialog).showModal();
-      }
+      dialog?.showModal();
     } else {
-      const dialog = dialogRef;
-      if (dialog) {
-        (dialog as CnDialog).close();
-      }
+      dialog?.close();
     }
   });
 
+  function handleClose() {
+    dialog?.close();
+    setMessage('');
+    dispatch('quote', '');
+    props.onClose?.();
+  }
+
+  function resetQuote(e: Event) {
+    logDebug('ReplyDialog', 'resetQuote', e);
+    dispatch('quote', '');
+  }
+
+  async function send(e: Event) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    logDebug('ReplyForm', 'send', e);
+
+    const reply = createReply({
+      markdownContent: message() || '',
+      owners: [uid()],
+      threadKey: props.threadKey,
+    });
+
+    logDebug(await addReply(reply));
+
+    handleClose();
+  }
+
   return (
-    <cn-dialog
-      ref={dialogRef}
-      title="Reply to Thread"
-      onClose={props.onClose}
-      open={props.open}
-    >
-      <p>[Reply Form goes here]</p>
-    </cn-dialog>
+    <dialog ref={dialog}>
+      <div class="header">
+        <button type="button" onClick={handleClose} aria-label="Close dialog">
+          <cn-icon noun="close" />
+        </button>
+        <h3>{t('disccussion:reply')}</h3>
+      </div>
+
+      {quote() && (
+        <div class="elevation-1 p-1 border-radius mb-1">
+          <button
+            type="button"
+            style="float: right;"
+            class="btn btn-clear"
+            onClick={resetQuote}
+          >
+            <cn-icon noun="delete" />
+          </button>
+          <p class="p-0 m-0">
+            <ProfileLink uid={quote()?.owners[0]} />
+          </p>
+          <MarkdownSection content={quote()?.markdownContent} />
+        </div>
+      )}
+
+      <form>
+        <textarea
+          placeholder={t('disccussion:replyPlaceholder')}
+          rows="5"
+          onBlur={(e) => setMessage(e.currentTarget.value)}
+        >
+          {message()}
+        </textarea>
+        <button type="submit">{t('disccussion:reply')}</button>
+      </form>
+    </dialog>
   );
 };
