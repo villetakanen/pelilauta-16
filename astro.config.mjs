4d0428df8ee181bdeeb4795c653a587632a9e7ce
Ville Takanen <vitkukissa@gmail.com>
2024-10-08 14:23:03 +0300

    feat: pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 import solidJs from '@astrojs/solid-js';
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro'
+import manifest from '/manifest.webmanifest';
 
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [solidJs(),
+    AstroPWA({
+      registerType: "autoUpdate",
+      manifest,
+      workbox: {
+        globDirectory: 'public',
+        globPatterns: [
+          '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+        ],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null,
+      }
+    })
+  ],
   output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true },
