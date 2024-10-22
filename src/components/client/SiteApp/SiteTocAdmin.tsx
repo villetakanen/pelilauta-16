43f466b06103330e6d9ee09564fe02e680ea3bdd
Ville Takanen <vitkukissa@gmail.com>
2024-10-22 13:30:57 +0300

    fix: finalized toc editing tool

--- src/components/client/SiteApp/SiteTocAdmin.tsx
+++ src/components/client/SiteApp/SiteTocAdmin.tsx
@@ -2,9 +2,12 @@
  * A solid-js component to manage the table of contents for a site.
  */
 
+import type { CnListItem } from '@11thdeg/cyan-next';
+import { SortableList } from '@client/shared/SortableList';
 import { db } from '@firebase/client';
 import { useStore } from '@nanostores/solid';
 import { SITES_COLLECTION_NAME, type Site } from '@schemas/SiteSchema';
+import type { CategoryRef } from '@schemas/SiteSchema';
 import { $uid } from '@stores/sessionStore';
 import { t } from '@utils/i18n';
 import { logDebug } from '@utils/logHelpers';
@@ -15,31 +18,64 @@ import { SiteTocRegenSection } from './SiteTocRegen';
 export const SiteTocAdmin: Component<{ site: Site }> = (props) => {
   const uid = useStore($uid);
   const visible = () => props.site.owners.includes(uid());
+  const [categories, setCategories] = createSignal<Array<CategoryRef>>(
+    props.site.pageCategories || [],
+  );
   const [newCategory, setNewCategory] = createSignal('');
 
-  const categories = () =>
-    props.site.pageCategories?.map((category) => ({
-      key: category.slug,
-      title: category.name,
+  const hasChanged = () => {
+    return (
+      JSON.stringify(categories()) !== JSON.stringify(props.site.pageCategories)
+    );
+  };
+
+  const categoryItems = () =>
+    categories().map((cat) => ({
+      key: cat.slug,
+      title: cat.name,
     }));
 
   async function addCategory() {
     if (!newCategory()) return;
-    const cats = [...(props.site.pageCategories || [])];
+    const cats = [...categories()];
     cats.push({
       name: newCategory(),
       slug: newCategory().toLowerCase().replace(/ /g, '-'),
     });
-    const siteRef = doc(db, SITES_COLLECTION_NAME, props.site.key);
+    setCategories(cats);
+    /*const siteRef = doc(db, SITES_COLLECTION_NAME, props.site.key);
     await updateDoc(siteRef, { pageCategories: cats });
     logDebug('Added category', newCategory());
+    window.location.reload();*/
+  }
+
+  async function reorderCategories(newOrder: Array<CnListItem>) {
+    const cats = newOrder.map((item) => ({
+      name: item.title,
+      slug: item.key,
+    }));
+    setCategories(cats);
+  }
+
+  async function saveToDB() {
+    const siteRef = doc(db, SITES_COLLECTION_NAME, props.site.key);
+    await updateDoc(siteRef, { pageCategories: categories() });
+    logDebug('Saved categories', categories());
     window.location.reload();
   }
 
   return visible() ? (
-    <div>
-      <h3>{t('site:toc.admin.title')}</h3>
-      <cn-sortable-list items={categories()} />
+    <div class="elevation-1 p-1 border-radius">
+      <h3>
+        <cn-icon noun="tools" />
+        &nbsp;
+        {t('site:toc.admin.title')}
+      </h3>
+      <SortableList
+        items={categoryItems()}
+        onItemsChanged={reorderCategories}
+        delete
+      />
       <div class="toolbar">
         <div class="grow">
           <input
@@ -53,6 +89,12 @@ export const SiteTocAdmin: Component<{ site: Site }> = (props) => {
         </div>
         <button type="button" onClick={addCategory}>
           <cn-icon noun="add" />
+        </button>
+      </div>
+
+      <div class="toolbar">
+        <button type="button" disabled={!hasChanged()} onClick={saveToDB}>
+          {t('save')}
         </button>
       </div>
 
