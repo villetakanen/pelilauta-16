a189b21be8d761ca3fd0eabb42cd6ae11779d732
Ville Takanen <vitkukissa@gmail.com>
2024-10-25 07:52:26 +0300

    fix: isolated thread app components for ssr conversion

--- src/components/client/threads/ThreadApp/ThreadApp.tsx
+++ src/components/client/threads/ThreadApp/ThreadApp.tsx
@@ -7,7 +7,6 @@ import { WithLoader } from '@client/shared/WithLoader';
 import { useStore } from '@nanostores/solid';
 import { subscribeThread } from '@stores/ThreadsApp';
 import { type Component, createMemo } from 'solid-js';
-import { ThreadInfoCard } from '../../ThreadsApp/ThreadInfoCard';
 import { ThreadArticle } from './ThreadArticle';
 
 export const ThreadApp: Component<{ thread: string }> = (props) => {
@@ -15,18 +14,8 @@ export const ThreadApp: Component<{ thread: string }> = (props) => {
   const loading = createMemo(() => !thread()?.key);
 
   return (
-    <>
-      <div class="content-columns">
-        <WithLoader loading={loading()}>
-          {thread() && <ThreadArticle thread={thread() || undefined} />}
-        </WithLoader>
-        {thread() && (
-          <ThreadInfoCard
-            thread={thread() || undefined}
-            author={thread()?.owners[0] || ''}
-          />
-        )}
-      </div>
-    </>
+    <WithLoader loading={loading()}>
+      {thread() && <ThreadArticle thread={thread() || undefined} />}
+    </WithLoader>
   );
 };
