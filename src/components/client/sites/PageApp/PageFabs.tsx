27ecfe105432cad376485eb1be13242df91eda18
Ville Takanen <vitkukissa@gmail.com>
2024-11-28 12:16:23 +0200

    fix: site and page mobile and social share polish

--- src/components/client/sites/PageApp/PageFabs.tsx
+++ src/components/client/sites/PageApp/PageFabs.tsx
@@ -1,14 +1,20 @@
 import { useStore } from '@nanostores/solid';
+import type { Page } from '@schemas/PageSchema';
 import type { Site } from '@schemas/SiteSchema';
 import { t } from '@utils/i18n';
 import type { Component } from 'solid-js';
 import { Portal } from 'solid-js/web';
 import { $uid } from 'src/stores/sessionStore';
 
+interface PageFabsProps {
+  page?: Page;
+  site: Site;
+}
+
 /**
  * Fabs available for the site owners and players
  */
-export const PageFabs: Component<{ pageKey: string; site: Site }> = (props) => {
+export const PageFabs: Component<PageFabsProps> = (props) => {
   const uid = useStore($uid);
 
   function editor() {
@@ -24,14 +30,14 @@ export const PageFabs: Component<{ pageKey: string; site: Site }> = (props) => {
             class="fab secondary small"
           >
             <cn-icon noun="add" small />
-            <span>{t('actions:create.page')}</span>
+            <span class="sm-hidden">{t('actions:create.page')}</span>
           </a>
           <a
-            href={`/sites/${props.site.key}/${props.pageKey}/edit`}
+            href={`/sites/${props.site.key}/${props.page?.key}/edit`}
             class="fab"
           >
             <cn-icon noun="edit" small />
-            <span>{t('actions:edit')}</span>
+            <span class="sm-hidden">{t('actions:edit')}</span>
           </a>
         </Portal>
       )}
