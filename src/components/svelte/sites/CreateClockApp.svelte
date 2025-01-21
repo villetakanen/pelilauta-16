980ac101dd13b1b53c122875eb2709395f1ea1cb
Ville Takanen <vitkukissa@gmail.com>
2025-01-21 10:32:54 +0200

    feat: clock creation ux v1

--- src/components/svelte/sites/CreateClockApp.svelte
+++ src/components/svelte/sites/CreateClockApp.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 import { parseClock } from '@schemas/ClockSchema';
-import { t } from '@utils/i18n';
 import { uid } from '@stores/sessionStore';
+import { t } from '@utils/i18n';
 /**
  * A Simple form to create a new clock
- * 
+ *
  * Clock labels are unique per site, so we'll need to mekanismiURI the label
  * and then check if it's unique before submitting the form
  */
@@ -21,18 +21,24 @@ function increaseTick(i: number) {
 function decreaseTick(i: number) {
   if (clock.ticks[i] > 1) clock.ticks[i] -= 1;
 }
-const clock = $state(parseClock({
-  ticks: [1, 1],
-  owners: [$uid],
-}));
+const clock = $state(
+  parseClock({
+    ticks: [2, 2, 1],
+    owners: [$uid],
+    label: 'New Clock',
+  }),
+);
 </script>
 
 <div class="content-columns">
-    <cn-story-clock name={clock.label} value={0}>
+  <section>
+    <cn-story-clock name={clock.label} value="0">
       {#each clock.ticks as tick}
         <cn-tick size={tick}></cn-tick>
       {/each}
     </cn-story-clock>
+    <p>{clock.label}</p>
+  </section>
 
   <form onsubmit={handleSubmit}>
     <h4>TICKS</h4>
