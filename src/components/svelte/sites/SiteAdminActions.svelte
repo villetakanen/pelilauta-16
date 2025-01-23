c1da63a721f78c15a3030ad1d06243ddc1f097d0
Ville Takanen <vitkukissa@gmail.com>
2025-01-23 14:02:26 +0200

    fix: site tray and fab visibility

---
+++ src/components/svelte/sites/SiteAdminActions.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+import type { Site } from '@schemas/SiteSchema';
+import { uid } from '@stores/sessionStore';
+import { t } from '@utils/i18n';
+import { systemToNoun } from '@utils/schemaHelpers';
+
+interface Props {
+  site: Site;
+}
+
+const { site }: Props = $props();
+const noun = $derived(systemToNoun(site.system));
+
+const showActions = $derived.by(() => {
+  if (site.owners.includes($uid)) return true;
+  // if (site.players?.includes($uid)) return true;
+  return false;
+});
+</script>
+
+{#if showActions}
+  <nav>
+    <ul>
+      <li>
+        <a href={`/sites/${site.key}/members`} class="tray-button">
+          <cn-icon noun="adventurer" xsmall></cn-icon>
+          <span>{t('site:members.title')}</span>
+        </a>
+      </li>
+      <li>
+        <a href={`/sites/${site.key}/settings`} class="tray-button">
+          <cn-icon noun="tools" xsmall></cn-icon>
+          <span>{t('site:settings.title')}</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+{:else}
+  <div class="flex items-center justify-center p-2" style="opacity:0.11">
+    <cn-icon noun={noun} large></cn-icon>
+  </div>
+{/if}
